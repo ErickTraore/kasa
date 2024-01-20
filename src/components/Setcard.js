43a09869e7e9f87bc6aff4642462d66316738622
Traore <traoreerick@gmail.com>
2024-01-20 09:31:39 +0100

    réglage du card

--- src/components/Setcard.js
+++ src/components/Setcard.js
@@ -7,19 +7,21 @@ import { Card } from 'react-bootstrap';
 
 function Setcard() {
   return (
-    <div className="img">
-        {docjson.map((doc, index) => (
-                <div className="img__doc" key={`${doc}-${index}`}>
-                      <Card className="img__doc__lumn">
-                          <Card.Img src={doc.cover} />
-                          <div className="img__doc__lumn__p2">
-                          </div>
-                          <div className="img__doc__lumn__p2__title">
-                            { doc.title}
-                          </div>
-                      </Card>
-                </div>
-        ))}
+    <div className="cardbody">
+      <div className="cardbody__card">
+          {docjson.map((doc, index) => (
+                  <div className="cardbody__card__doc" key={`${doc}-${index}`}>
+                        <Card className="cardbody__card__doc__lumn">
+                            <Card.Img src={doc.cover} />
+                            <div className="cardbody__card__doc__lumn__p2">
+                            </div>
+                            <div className="cardbody__card__doc__lumn__p2__title">
+                              { doc.title}
+                            </div>
+                        </Card>
+                  </div>
+          ))}
+      </div>
     </div>
 
   );
